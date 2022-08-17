1f06d27ea81fe7f8a5aad895cc089c984b9ad8dd
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-08-17 10:42:04 +0100

    Add additional icons to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,79 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assetts/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+    useCurrentUser,
+    useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
 
-    const loggedInIcons = <>{currentUser?.username}</>
+    const [expanded, setExpanded] = useState(false);
+    const ref = useRef(null);
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setExpanded(false);
+            }
+        };
+        document.addEventListener('mouseup', handleClickOutside)
+        return () => {
+            document.removeEventListener('mouseup', handleClickOutside)
+        }
+    }, [ref]);
+
+    const handleSignOut = async () => {
+        try {
+        await axios.post("dj-rest-auth/logout/");
+        setCurrentUser(null);
+        } catch (err) {
+        console.log(err);
+        }
+    };
+
+    const addPostIcon = (
+        <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/posts/create"
+        >
+        <i className="far fa-plus-square"></i>Add Post
+        </NavLink>
+    );
+    const loggedInIcons = (
+        <>
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/feed"
+        >
+            <i className="fas fa-stream"></i>Feed
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/liked"
+        >
+            <i className="far fa-heart"></i>Liked
+        </NavLink>
+        <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+            <i className="fas fa-sign-out-alt"></i>Sign out
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            to={`/profiles/${currentUser?.profile_id}`}
+            onClick={() => {}}
+        >
+            <Avatar src={currentUser?.profile_image} text="profile" height={40} />
+        </NavLink>
+        </>
+    );
     const loggedOutIcons = (
         <>
         <NavLink
@@ -28,14 +93,24 @@ const NavBar = () => {
         </>
     );
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar
+        expanded={expanded}
+        className={styles.NavBar}
+        expand="md"
+        fixed="top"
+        >
         <Container>
             <NavLink to="/">
             <Navbar.Brand>
                 <img src={logo} alt="logo" height="45" />
             </Navbar.Brand>
             </NavLink>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            {currentUser && addPostIcon}
+            <Navbar.Toggle
+            ref={ref}
+            onClick={() => setExpanded(!expanded)}
+            aria-controls="basic-navbar-nav"
+            />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
                 <NavLink
