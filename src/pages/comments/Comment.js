087b2135569e21afb5d95512393310bc0fb047cb
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-09-10 03:04:07 +0100

    Optimize your bootstrap imports

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Media } from "react-bootstrap";
+import Media from "react-bootstrap/Media";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { MoreDropdown } from "../../components/MoreDropdown";
